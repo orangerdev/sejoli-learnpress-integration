3d03ee36301acff47e52eb3e542a2a0f8414fd9e
Ridwan Arifandi <hi@ridwanarifandi.com>
2020-01-07 13:48:46 +0700

    CLI command to simulate learnpress order creation

--- cli/simulation.php
+++ cli/simulation.php
@@ -4,6 +4,12 @@ namespace SejoliLP\CLI;
 
 class Simulation
 {
+    protected $user_id = 0;
+
+    public function set_user_id() {
+        return $this->user_id;
+    }
+
     /**
      * Get all active subscription and product
      *
@@ -22,6 +28,12 @@ class Simulation
     public function order(array $args) {
 
         list( $user_id, $course_id ) = $args;
-        __debug($user_id, $course_id);
+        $this->user_id = $user_id;
+
+        LP()->cart->add_to_cart($course_id);
+
+        add_filter('learn-press/checkout/default-user', array($this, 'set_user_id'));
+
+        __debug(LP()->checkout()->create_order());
     }
 }
