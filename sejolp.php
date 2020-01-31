9f62ca4ec7cb4080a366dc5544c1703e364e199c
Ridwan Arifandi <hi@ridwanarifandi.com>
2020-01-31 17:33:23 +0700

    add conditional to check if learnpress is already active or no

--- sejolp.php
+++ sejolp.php
@@ -51,68 +51,79 @@ function sejolp_check_sejoli() {
 
 }
 
+if( !class_exists( 'LearnPress' )) :
 
-/**
- * Currently plugin version.
- * Start at version 1.0.0 and use SemVer - https://semver.org
- * Rename this for your plugin and update it as you release new versions.
- */
-define( 'SEJOLP_VERSION', '1.0.0' );
-define( 'SEJOLP_DIR',	 plugin_dir_path(__FILE__));
-define( 'SEJOLP_URL',	 plugin_dir_url(__FILE__));
+	add_action('admin_notices', 'sejolp_no_learnpress_functions');
 
-/**
- * The code that runs during plugin activation.
- * This action is documented in includes/class-sejolp-activator.php
- */
-function activate_sejolp() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-sejolp-activator.php';
-	SejoliLP_Activator::activate();
-}
+	function sejolp_no_learnpress_functions() {
+		?><div class='notice notice-error'>
+		<p><?php _e('Anda belum menginstall atau mengaktifkan LearnPress terlebih dahulu.', 'sejolp'); ?></p>
+		</div><?php
+	}
 
-/**
- * The code that runs during plugin deactivation.
- * This action is documented in includes/class-sejolp-deactivator.php
- */
-function deactivate_sejolp() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-sejolp-deactivator.php';
-	SejoliLP_Deactivator::deactivate();
-}
+else :
+	/**
+	 * Currently plugin version.
+	 * Start at version 1.0.0 and use SemVer - https://semver.org
+	 * Rename this for your plugin and update it as you release new versions.
+	 */
+	define( 'SEJOLP_VERSION', '1.0.0' );
+	define( 'SEJOLP_DIR',	 plugin_dir_path(__FILE__));
+	define( 'SEJOLP_URL',	 plugin_dir_url(__FILE__));
 
-register_activation_hook( __FILE__, 'activate_sejolp' );
-register_deactivation_hook( __FILE__, 'deactivate_sejolp' );
+	/**
+	 * The code that runs during plugin activation.
+	 * This action is documented in includes/class-sejolp-activator.php
+	 */
+	function activate_sejolp() {
+		require_once plugin_dir_path( __FILE__ ) . 'includes/class-sejolp-activator.php';
+		SejoliLP_Activator::activate();
+	}
 
-/**
- * The core plugin class that is used to define internationalization,
- * admin-specific hooks, and public-facing site hooks.
- */
-require plugin_dir_path( __FILE__ ) . 'third-parties/autoload.php';
-require plugin_dir_path( __FILE__ ) . 'includes/class-sejolp.php';
+	/**
+	 * The code that runs during plugin deactivation.
+	 * This action is documented in includes/class-sejolp-deactivator.php
+	 */
+	function deactivate_sejolp() {
+		require_once plugin_dir_path( __FILE__ ) . 'includes/class-sejolp-deactivator.php';
+		SejoliLP_Deactivator::deactivate();
+	}
 
-/**
- * Begins execution of the plugin.
- *
- * Since everything within the plugin is registered via hooks,
- * then kicking off the plugin from this point in the file does
- * not affect the page life cycle.
- *
- * @since    1.0.0
- */
-function run_sejolp() {
+	register_activation_hook( __FILE__, 'activate_sejolp' );
+	register_deactivation_hook( __FILE__, 'deactivate_sejolp' );
 
-	$plugin = new SejoliLP();
-	$plugin->run();
+	/**
+	 * The core plugin class that is used to define internationalization,
+	 * admin-specific hooks, and public-facing site hooks.
+	 */
+	require plugin_dir_path( __FILE__ ) . 'third-parties/autoload.php';
+	require plugin_dir_path( __FILE__ ) . 'includes/class-sejolp.php';
 
-}
+	/**
+	 * Begins execution of the plugin.
+	 *
+	 * Since everything within the plugin is registered via hooks,
+	 * then kicking off the plugin from this point in the file does
+	 * not affect the page life cycle.
+	 *
+	 * @since    1.0.0
+	 */
+	function run_sejolp() {
 
-require_once(SEJOLP_DIR . 'third-parties/yahnis-elsts/plugin-update-checker/plugin-update-checker.php');
+		$plugin = new SejoliLP();
+		$plugin->run();
 
-$update_checker = Puc_v4_Factory::buildUpdateChecker(
-	'https://github.com/orangerdev/sejoli-learnpress-integration',
-	__FILE__,
-	'sejoli-learnpress'
-);
+	}
 
-$update_checker->setBranch('master');
+	require_once(SEJOLP_DIR . 'third-parties/yahnis-elsts/plugin-update-checker/plugin-update-checker.php');
 
-run_sejolp();
+	$update_checker = Puc_v4_Factory::buildUpdateChecker(
+		'https://github.com/orangerdev/sejoli-learnpress-integration',
+		__FILE__,
+		'sejoli-learnpress'
+	);
+
+	$update_checker->setBranch('master');
+
+	run_sejolp();
+endif;
