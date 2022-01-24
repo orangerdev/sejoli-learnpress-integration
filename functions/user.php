4eaba4c71191707f5182b6ed97453496d83e3636
asdzakky <asd.zakky@gmail.com>
2022-01-24 21:07:36 +0700

    fix[bug get_id()] bug function get_id() in new version learnpress plugin

--- functions/user.php
+++ functions/user.php
@@ -9,22 +9,31 @@ function sejolilp_get_user_purchased_courses($user_id = 0) {
 
     $courses = array();
     $profile = learn_press_get_profile($user_id);
+    $user    = learn_press_get_user( $user_id );
     $query   = $profile->query_courses( 'purchased' );
 
     if( $query['items'] ) :
 
         foreach($query['items'] as $user_course) :
 
-            $id           = $user_course->get_id();
-            $courses[$id] = array(
-                'date'          => $user_course->get_start_time(),
-                'result'        => $user_course->get_percent_result(),
-                'status'        => $user_course->get_results( 'status' )
-            );
+            $course_data = $user->get_course_data( $user_course );
+
+            if ( $course_data ) {
+                $courses[] = array(
+                    'id'         => $user_course ?? '',
+                    'graduation' => ! empty( $course_data->get_graduation() ) ? $course_data->get_graduation() : '',
+                    'status'     => ! empty( $course_data->get_status() ) ? $course_data->get_status() : '',
+                    'start_time' => lp_jwt_prepare_date_response( $course_data->get_start_time() ? $course_data->get_start_time()->toSql( false ) : '' ),
+                    'end_time'   => lp_jwt_prepare_date_response( $course_data->get_end_time() ? $course_data->get_end_time()->toSql( false ) : '' ),
+                    'expiration' => lp_jwt_prepare_date_response( $course_data->get_expiration_time() ? $course_data->get_expiration_time()->toSql( false ) : '' ),
+                    'results'    => $course_data->calculate_course_results(),
+                );
+            }
 
         endforeach;
 
     endif;
 
     return $courses;
+
 }
